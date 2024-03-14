08966f6c31a50b2c26d319723494cdf3aa251169
ereminiu <ereminiu@mail.ru>
2024-03-15 01:22:11 +0300

    Make /add-actor idempotent

--- internal/db/repositories/actor.go
+++ internal/db/repositories/actor.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"database/sql"
+	"errors"
 )
 
 type ActorRepository struct {
@@ -15,7 +16,7 @@ func NewActorRepository(db *sql.DB) *ActorRepository {
 }
 
 func (ar *ActorRepository) CreateActor(name, gender, birthday string) (int, error) {
-	sqlQuery := `INSERT INTO actors (name, gender, birthday) values ($1, $2, $3) RETURNING id`
+	sqlQuery := `INSERT INTO actors (name, gender, birthday) values ($1, $2, $3) ON CONFLICT DO NOTHING RETURNING id`
 	tx, err := ar.db.Begin()
 	if err != nil {
 		return -1, err
@@ -25,10 +26,30 @@ func (ar *ActorRepository) CreateActor(name, gender, birthday string) (int, erro
 	row := tx.QueryRow(sqlQuery, name, gender, birthday)
 	err = row.Scan(&actorId)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			actorId, err = ar.getActorId(tx, name, gender, birthday)
+			if err != nil {
+				tx.Rollback()
+				return -1, err
+			}
+			return actorId, tx.Commit()
+		}
 		tx.Rollback()
 		return -1, err
 	}
 	return actorId, tx.Commit()
+}
+
+func (ar *ActorRepository) getActorId(tx *sql.Tx, name, gender, birthday string) (int, error) {
+	var actorId int
+	sqlQuery := `SELECT id FROM actors WHERE name=$1 AND gender=$2 AND birthday=$3`
+	row := tx.QueryRow(sqlQuery, name, gender, birthday)
+	err := row.Scan(&actorId)
+	if err != nil {
+		tx.Rollback()
+		return -1, err
+	}
+	return actorId, nil
 }
 
 func (ar *ActorRepository) ChangeField(field, newValue string) error {
